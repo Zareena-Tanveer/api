c3f21a84450a7c91548ca891284e85e077b2f2f4
Zari-Tanveer <zaritanveer1999@gmail.com>
2021-02-03 18:46:23 +0500

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API route for Reviews
+
+Route::group(['prefix'=>'product'],function(){
+    Route::apiResource('{product}/review','ReviewController');
+});
+// API route for product 
+
+Route::apiResource('products','ProductController');
+
+
