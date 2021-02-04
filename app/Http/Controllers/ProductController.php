06431b9a33157c4f202a4407b7f96280473b1f86
Zari-Tanveer <zaritanveer1999@gmail.com>
2021-02-04 16:23:06 +0500

    Product Insertion through API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,15 @@ use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\SingleProductResource;
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
-    /**
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+    /** 
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -34,9 +40,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+            'data' => new SingleProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
