5e6e4cd1f5f618f85b128a5de3b5760e782d48db
Zari-Tanveer <zaritanveer1999@gmail.com>
2021-02-07 22:43:13 +0500

    Adding Custom Exception Trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Exceptions\ExceptionTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -53,18 +52,8 @@ class Handler extends ExceptionHandler
         // bool expectsJson() returns either true or false
         // Determine if the current request probably expects a JSON response.
         if($request->expectsJson()){
-            if ($exception instanceof ModelNotFoundException) {
-                return response()->json([
-                    'errors' => 'Model Not Found'
-                ],Response::HTTP_NOT_FOUND);
-             }
-            if($exception instanceof NotFoundHttpException){
-                return response()->json([
-                    'errors' => 'Incorrect Route'
-                ],Response::HTTP_NOT_FOUND);
-            }
+            return $this->apiException($request,$exception);
         }
-        dd($exception);
         return parent::render($request, $exception);
     }
 }
